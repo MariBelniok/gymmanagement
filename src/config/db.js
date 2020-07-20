5356182963426fd8758b527c1242a5b9abe73885
MariBelniok <61606548+MariBelniok@users.noreply.github.com>
2020-07-20 19:54:43 -0300

    Delete db.js

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg')
-
-module.exports = new Pool({
-    user:'marianna',
-    password: '170681av',
-    host: 'localhost',
-    port: 5432,
-    database: 'gymmanager'
-})
